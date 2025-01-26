255467853fa8abb3e01d38241068d878bc14e1b8
Chirag29103 <chirag.chauhan_cs22@gla.ac.in>
2025-01-26 14:23:54 +0530

    Backend init

--- project/src/About.jsx
+++ project/src/About.jsx
@@ -4,7 +4,7 @@ function About() {
 
     return (
         <>
-            <div className="box-border w-full p-6 h-[50vh] scroll-mt-[14vh] " id='about'>
+            <div className="box-border text-center min-h-[86vh] w-full p-6 h-[50vh] scroll-mt-[14vh] " id='about'>
                 <h1 className="text-4xl pb-4 font-semibold">About ExpenseTracker</h1>
                 <p className="text-lg">ExpenseTracker is a sophisticated financial management solution designed for individuals and businesses seeking to optimize their financial health. Our platform combines cutting-edge technology with user-friendly interfaces to provide a comprehensive tool for expense tracking, budget planning, and financial analysis. With ExpenseTracker, you're empowered to make informed decisions and achieve your financial goals with confidence.
                 </p>
--- project/src/SignUp.jsx
+++ project/src/SignUp.jsx
@@ -4,24 +4,25 @@ import Footer from "./Footer";
 function SignUp(){
     return (
         <>
-        <div className='w-full h-[85.5vh] bg-gradient-to-b from-black via-slate-800 to-slate-700 flex flex-col items-center gap-10'>
+        <div className='w-full h-[85.5vh] bg-gradient-to-b from-slate-500 to-slate-900 bg-opacity-40
+        flex flex-col items-center gap-10'>
             <h1 className='text-4xl text-orange-400 pt-4'>SignUp</h1>
-            <form className='flex flex-col gap-5 bg-white bg-opacity-75 rounded-md 
-            shadow-lg w-[50vw] p-10'>
+            <form className='flex flex-col gap-5 rounded-md bg-gradient-to-b from-slate-700 via-slate-800 to-black text-orange-400
+            shadow-lg w-[50vw] p-10 justify-center items-center'>
                 <div className='flex flex-col items-center gap-5'>
-                    <div className='flex flex-col gap-5'>
+                    <div className='flex flex-col gap-2'>
                         <label htmlFor="mail">email or username:</label>
                         <input type='email' className='rounded-md' required name='mail' />
                     </div>
-                    <div className='flex flex-col gap-5'>
+                    <div className='flex flex-col gap-2'>
                         <label htmlFor="password">password:</label>
                         <input type='password' className='rounded-md' required name='password' />
                     </div>
-                    <div className='flex flex-col gap-5'>
+                    <div className='flex flex-col gap-2'>
                         <label htmlFor="password">confirm password:</label>
                         <input type='password' className='rounded-md' required name='password' />
                     </div>
-                    <button className="text-lg items bg-orange-400 rounded-md w-[15vw] py-1"
+                    <button className="text-lg text-white items bg-orange-400 rounded-md w-[15vw] pt-0.5 pb-1"
                         type='submit'>Login</button>
                 </div>
             </form>
